b7b424199667af7beece8dbc2a849ab2adfc505c
Avery Lanier <averytlanier@icloud.com>
2025-09-23 11:59:49 -0500

    Box grab & carry system implemented

--- Assets/Environment/Scripts/ColorBox.cs
+++ Assets/Environment/Scripts/ColorBox.cs
@@ -4,7 +4,26 @@ using UnityEngine;
 
 public class ColorBox : MonoBehaviour
 {
-    
 
+    [HideInInspector] public Rigidbody2D rb;
+    [HideInInspector] public BoxCollider2D boxCollider;
+
+    public bool isCarried;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0f;
+        rb.freezeRotation = true;
+        rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+
+        boxCollider = GetComponent<BoxCollider2D>();
+        ChangeColliderEnable(true);
+    }
+
+    public void ChangeColliderEnable(bool enable)
+    {
+        boxCollider.enabled = enable;
+    }
 
 }
--- Assets/Player/Scripts/PlayerInteraction.cs
+++ Assets/Player/Scripts/PlayerInteraction.cs
@@ -6,5 +6,97 @@ using UnityEngine.InputSystem;
 [RequireComponent (typeof(Rigidbody2D))]
 public class PlayerInteraction : MonoBehaviour
 {
-    
+    [Header("Input")]
+    public InputActionReference interactAction;
+    public InputActionReference moveAction;
+
+    [Header("Interaction Settings")]
+    public LayerMask boxLayer;
+    public float interactRange = 10f;
+    public Transform carryPoint;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private ColorBox carriedBox;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        if (interactAction != null) interactAction.action.started += OnInteractPressed;
+        if (moveAction != null) moveAction.action.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        if (moveAction != null) moveAction.action.canceled += ctx => moveInput = Vector2.zero;
+
+        interactAction?.action.Enable();
+        moveAction?.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        if (interactAction != null) interactAction.action.started -= OnInteractPressed;
+
+        interactAction?.action.Disable();
+        moveAction?.action.Disable();
+    }
+
+    private void OnInteractPressed(InputAction.CallbackContext context)
+    {
+        if (carriedBox == null)
+        {
+            TryPickupBox();
+        }
+        else
+        {
+            DropBox();
+        }
+    }
+
+    private void TryPickupBox()
+    {
+        Vector2 dir = GetFacingDirection();
+        Collider2D hit = Physics2D.OverlapCircle(rb.position, interactRange, boxLayer);
+
+        if (hit != null)
+        {
+            ColorBox box = hit.GetComponent<ColorBox>();
+            if (box != null && !box.isCarried)
+            {
+                carriedBox = box;
+                carriedBox.isCarried = true;
+
+                // Disable physics while carried
+                carriedBox.rb.bodyType = RigidbodyType2D.Kinematic;
+                carriedBox.rb.simulated = false;
+                carriedBox.ChangeColliderEnable(false);
+
+                // Parent to player carry point
+                carriedBox.transform.SetParent(carryPoint);
+                carriedBox.transform.localPosition = Vector3.zero;
+            }
+        }
+    }
+
+    private void DropBox()
+    {
+        if (carriedBox == null) return;
+
+        carriedBox.isCarried = false;
+        carriedBox.transform.SetParent(null);
+
+        // Re-enable physics
+        carriedBox.rb.bodyType = RigidbodyType2D.Kinematic;
+        carriedBox.rb.simulated = true;
+        carriedBox.ChangeColliderEnable(true);
+
+        carriedBox = null;
+    }
+
+    private Vector2 GetFacingDirection()
+    {
+        if (moveInput.sqrMagnitude > 0.01f) return moveInput.normalized;
+        return Vector2.up;
+    }
 }
